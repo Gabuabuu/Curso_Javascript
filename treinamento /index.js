c6b07f86ac0c2f041557fe9ecb58b1138b46f4c2
Gabu <gabydavilaneves@gmail.com>
2022-02-04 12:22:45 -0300

    correção de escrita

--- treinamento /index.js
+++ treinamento /index.js
@@ -2,9 +2,9 @@ function mostra(frase) {
   document.write(frase);
 }
 
-var nota1 = Number(parseInt(prompt("Qual nova você tirou no primeiro trimestre?")));
-var nota2 = Number(parseInt(prompt("Qual nova você tirou no segundo trimestre?")));
-var nota3 = Number(parseInt(prompt("Qual nova você tirou no terceiro trimestre?")));
+var nota1 = Number(parseInt(prompt("Qual nota você tirou no primeiro trimestre?")));
+var nota2 = Number(parseInt(prompt("Qual nota você tirou no segundo trimestre?")));
+var nota3 = Number(parseInt(prompt("Qual nota você tirou no terceiro trimestre?")));
 
 var soma = ((nota1 + nota2 + nota3) /3).toFixed();
 
