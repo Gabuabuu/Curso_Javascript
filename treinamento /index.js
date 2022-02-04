a0ea1ccde384a96d3be73d50450d149965e84cce
Gabu <gabydavilaneves@gmail.com>
2022-02-04 10:51:14 -0300

    separating javascript from html

---
+++ treinamento /index.js
@@ -0,0 +1,16 @@
+function mostra(frase) {
+  document.write(frase);
+}
+
+var nota1 = Number(parseInt(prompt("Qual nova você tirou no primeiro trimestre?")));
+var nota2 = Number(parseInt(prompt("Qual nova você tirou no segundo trimestre?")));
+var nota3 = Number(parseInt(prompt("Qual nova você tirou no terceiro trimestre?")));
+
+var soma = ((nota1 + nota2 + nota3) /3).toFixed();
+
+if (soma < 5) {
+  mostra(`Sua nota foi de ${soma} e por isso você reprovou`);
+} else {
+  soma >= 5
+  mostra(`Parabéns! sua nota foi de ${soma} e você passou de ano`);
+}
