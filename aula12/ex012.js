277673ee2c923f39c9d762531686305f693a8203
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-03-27 12:56:26 -0300

    exercício 1

---
+++ aula12/ex012.js
@@ -0,0 +1,13 @@
+var hour = new Date()
+var dayW = hour.getDay()
+
+switch(dayW) {
+  case 0: console.log('Domingo')
+  break
+  
+  case 1: console.log('Segunda')
+  break
+  case 2: console.log('Terça')
+  break 
+  
+}
