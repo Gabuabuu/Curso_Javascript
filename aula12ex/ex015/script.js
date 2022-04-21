a0b46d6dc68e3460b83d0578558c1000d1535848
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-04-21 17:46:19 -0300

    adicionando função ao botão

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,10 @@
-alert('Olá')
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  
+  if(fano.value.length == 0 || fano.value > ano) {
+    alert('Preencha os dados e tente novamente ')
+  }
+}
