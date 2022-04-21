72abf6e6dcfa65a68a9beea095718cdff0cfa88e
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-04-21 19:30:03 -0300

    adicionando fotos

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -14,8 +14,27 @@ function verificar() {
     img.setAttribute('id', 'foto')//seta um atributo para minha variavel
     if(fsex[0].checked)/*(fsex[0].checked) Seleciona o primeiro gênero, checked serve para selecionar a opcao selecionada*/ {
       genero = 'Homem'
+        if (idade >=0 && idade < 10) {
+          //Criança
+          img.setAttribute('src', '')
+        } else if (idade < 21 ){
+          //Jovem
+        } else if (idade < 50) {
+          //adulto
+        } else {
+          //idoso
+        }
     } else if (fsex[1].checked) {
       genero = 'Mulher'
+      if (idade >=0 && idade < 10) {
+          //Criança 
+        } else if (idade < 21 ){
+          //Jovem
+        } else if (idade < 50) {
+          //adulto
+        } else {
+          //idoso
+        }
     }
     res.style.textAlign = 'center'
     res.innerHTML = `Detectamos que você é ${genero} e tem ${idade} anos`
