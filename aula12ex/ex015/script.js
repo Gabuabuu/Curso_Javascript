f85b52e12c1136600043256cd880e4890026f527
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-04-21 17:58:05 -0300

    adicionando função ao botão

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -4,7 +4,11 @@ function verificar() {
   var fano = document.getElementById('txtano')
   var res = document.getElementById('res')
   
-  if(fano.value.length == 0 || fano.value > ano) {
-    alert('Preencha os dados e tente novamente ')
+  if(fano.value.length == 0 || Number(fano.value) > ano) {
+    alert('[ERRO] Preencha os dados e tente novamente')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    res.innerHTML = `A sua idade é ${idade}`
   }
 }
