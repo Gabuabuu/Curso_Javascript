49d490bbd8846331b71f4a92d9766bc3cfc11fce
gabydavilaneves@gmail.com <gabydavilaneves@gmail.com>
2022-04-21 19:44:45 -0300

    Adicionando foto conforme a idade

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -16,27 +16,36 @@ function verificar() {
       genero = 'Homem'
         if (idade >=0 && idade < 10) {
           //Criança
-          img.setAttribute('src', '')
+          img.setAttribute('src', 'foto-bebe-m.png')
         } else if (idade < 21 ){
           //Jovem
+          img.setAttribute('src', 'foto-jovem-m.png')
         } else if (idade < 50) {
           //adulto
+          img.setAttribute('src', 'foto-adulto-m.png')
         } else {
           //idoso
+          img.setAttribute('src', 'foto-idoso-m.png')
         }
     } else if (fsex[1].checked) {
       genero = 'Mulher'
-      if (idade >=0 && idade < 10) {
-          //Criança 
-        } else if (idade < 21 ){
+      nero = 'Homem'
+        if (idade >= 0 && idade < 10) {
+          //Criança
+          img.setAttribute('src', 'foto-bebe-f.png')
+        } else if (idade < 21) {
           //Jovem
+          img.setAttribute('src', 'foto-jovem-f.png')
         } else if (idade < 50) {
           //adulto
+          img.setAttribute('src', 'foto-adulto-f.png')
         } else {
           //idoso
+          img.setAttribute('src', 'foto-idoso-f.png')
         }
     }
     res.style.textAlign = 'center'
     res.innerHTML = `Detectamos que você é ${genero} e tem ${idade} anos`
+    res.appendChild(img)//Adiciona o elemento "img" depois do res.innerHTML
   }
 }
